4fb0d623ce16f028fc31054c580add5b26bab751
ChrisLi <chris.li@datarobot.com>
2020-05-22 11:56:04 -0400

    coordinator setup

--- iOS/Controller/MapController.swift
+++ iOS/Controller/MapController.swift
@@ -10,23 +10,44 @@ import MapKit
 import SwiftUI
 
 @available(iOS 13.0.0, *)
-struct MapView: UIViewRepresentable {
-    func makeUIView(context: Context) -> MKMapView {
-        let mapView = MKMapView()
-        return mapView
-    }
-
-    func updateUIView(_ view: MKMapView, context: Context) {
-        let coordinate = CLLocationCoordinate2D(latitude: 12.9716, longitude: 77.5946)
-        let span = MKCoordinateSpan(latitudeDelta: 2.0, longitudeDelta: 2.0)
-        let region = MKCoordinateRegion(center: coordinate, span: span)
-        view.setRegion(region, animated: true)
+struct ContentView: View {
+    var body: some View {
+        return MapView().edgesIgnoringSafeArea(.all)
     }
 }
 
 @available(iOS 13.0.0, *)
-struct MapView_Previews: PreviewProvider {
+struct MapView: UIViewRepresentable {
+    func makeUIView(context: Context) -> MKMapView {
+        let mapView = MKMapView()
+        mapView.delegate = context.coordinator
+        return mapView
+    }
+
+    func updateUIView(_ view: MKMapView, context: Context) {
+        let coordinate = CLLocationCoordinate2D(latitude: 42.361145, longitude: -71.057083)
+        let span = MKCoordinateSpan(latitudeDelta: 2.0, longitudeDelta: 2.0)
+        let region = MKCoordinateRegion(center: coordinate, span: span)
+        view.setRegion(region, animated: true)
+    }
+    
+    func makeCoordinator() -> MapViewCoordinator {
+        MapViewCoordinator(mapView: self)
+    }
+}
+
+@available(iOS 13.0.0, *)
+class MapViewCoordinator: NSObject, MKMapViewDelegate {
+    let mapView: MapView
+
+    init(mapView: MapView) {
+        self.mapView = mapView
+    }
+}
+
+@available(iOS 13.0.0, *)
+struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
-        MapView()
+        ContentView()
     }
 }
